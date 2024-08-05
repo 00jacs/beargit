b77e14614d98ab2071dd3b6fcaee17645472b996
jacobxcoder <gillbatesdev@gmail.com>
2024-08-05 11:14:54 +0200

    uuid: add simple uuid generator function for commit IDs

--- main.c
+++ main.c
@@ -1,7 +1,27 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <uuid/uuid.h>
+
+char* generate_random_uuid() {
+  uuid_t binuuid;
+  uuid_generate_random(binuuid);
+
+  // each uuid has 36 characters
+  // so we need to reserve 37 (include \0)
+  char *uuid = malloc(37); 
+
+  // convert UUID from binary representation to a string
+  uuid_unparse(binuuid, uuid);
+  return uuid;
+}
+
 
 int main(int argc, char *argv[]) {
+  char* uuid = generate_random_uuid();
+  printf("uuid: %s\n", uuid);
+
+
   if (argc != 2) {
     printf("Usage: %s <command>\n", argv[0]);
     return 1;
