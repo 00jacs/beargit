02678fab5ba1b660ea386be44658133ef03697ce
jacobxcoder <gillbatesdev@gmail.com>
2024-08-05 10:57:09 +0200

    init: add build.sh for automatic compilation of beargit.out

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# This script will take of building (compiling) the beargit program
+
+echo "Building 'beargit'..."
+cc main.c -o beargit.out
+echo "Successfully built beargit.out. You can run it with ./beargit.out"
